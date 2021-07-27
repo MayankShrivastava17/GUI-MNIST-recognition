c39e5ae810992b209d2dd5aec7a1adaca2d5365b
Mayank Shrivastava <mayanksri910@gmail.com>
2021-07-28 00:00:56 +0530

    Added train folder

---
+++ train/model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation="relu"),
+    Dense(10, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_test, y_test),
+    epochs=10
+)
+
+model.save('./model/gui-mnist.h5')
